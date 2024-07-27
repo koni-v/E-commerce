2bc6b96c30d06db75205df7da9b6ae53ebff78b6
koni-v <k.vidovic.mk@gmail.com>
2024-07-27 19:46:17 +0200

    Adjustments in the design

--- components/Layout.js
+++ components/Layout.js
@@ -16,7 +16,7 @@ export default function Layout({children}) {
   return(
     <div className="bg-blue-900 min-h-screen flex">
       <Nav />
-      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg">
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
         {children}
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import { useSession } from "next-auth/react";
 import Layout from "../components/Layout";
 
 export default function Home() {
+  const {data: session} = useSession()
+  console.log(session);
   return(
-    <Layout>test</Layout>
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex gap-1 bg-gray-200 text-black rounded-lg">
+          <img src={session?.user?.image} alt="" className="w-6 h-6 rounded-lg"/>
+          <span className="px-2">
+            {session?.user?.name}
+          </span>
+        </div>
+      </div>
+    </Layout>
   )
 }
